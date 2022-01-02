d9c1b772e38b7315fe14e9063ca7a457fa75155f
coder9a <rajsinghbalhara@gmail.com>
2022-01-02 16:19:47 +0530

    new commit

--- market/models.py
+++ market/models.py
@@ -1,6 +1,11 @@
+from flask_wtf.form import FlaskForm
+from wtforms.fields.simple import StringField
+from wtforms.validators import DataRequired
 from market import db
+from market import bcrypt
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
@@ -9,6 +14,17 @@ class User(db.Model):
     items = db.relationship('Item', backref='owned_user', lazy=True)
     def __repr__(self):
         return f'Item {self.name}'
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
--- market/routes.py
+++ market/routes.py
@@ -2,7 +2,7 @@ from flask.helpers import url_for,flash
 from werkzeug.utils import redirect
 from market import app
 from flask import render_template
-from market.forms import RegistrationForm
+from market.forms import RegistrationForm, LoginForm
 from market.models import Item, User
 from market import db
 
@@ -20,7 +20,7 @@ def registerPage():
     form = RegistrationForm()
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
-                                password_hash=form.password1.data,
+                                password=form.password1.data,
                                 email_address=form.email_address.data)
         db.session.add(user_to_create)
         db.session.commit()
@@ -30,3 +30,12 @@ def registerPage():
         for err_msg in form.errors.values():
             flash(f'There is an error with creating a user: {err_msg}', category='danger')
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['POST', 'GET'])
+def loginPage():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_user=form.password.data):
+            login_user(attempted_user)
+    return render_template('login.html', form=form)
